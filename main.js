2dd79f7018f6ff106348fe584becae67858b2208
RobTrb <robert.trb@gmail.com>
2024-11-07 13:35:54 +0100

    just removed a row in js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const hamMenu = document.querySelector(".header_hamMenu");
 const mobileMenuCont = document.querySelector(".header_mobileMenuContainer");
 const mobileMenu = document.querySelector(".header_mobileMenu");
 
-
 hamMenu.addEventListener("click", () => {
     hamMenu.classList.toggle("active");
     mobileMenuCont.classList.toggle("active");
