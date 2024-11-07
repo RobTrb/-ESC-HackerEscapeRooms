f89f12fe09c21797c9ce12f44ff5ac64763357b9
RobTrb <robert.trb@gmail.com>
2024-11-07 13:17:08 +0100

    finished mob menu styling and added JS

---
+++ main.js
@@ -0,0 +1,10 @@
+const hamMenu = document.querySelector(".header_hamMenu");
+const mobileMenuCont = document.querySelector(".header_mobileMenuContainer");
+const mobileMenu = document.querySelector(".header_mobileMenu");
+
+
+hamMenu.addEventListener("click", () => {
+    hamMenu.classList.toggle("active");
+    mobileMenuCont.classList.toggle("active");
+    mobileMenu.classList.toggle("active");    
+});
