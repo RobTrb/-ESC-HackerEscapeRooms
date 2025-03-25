b1910d5338fcd281b719b6cf9490612d60bd7007
RobTrb <robert.trb@gmail.com>
2025-03-25 14:49:58 +0100

    just added a testing fetch function

--- main.js
+++ main.js
@@ -7,3 +7,20 @@ hamMenu.addEventListener("click", () => {
     mobileMenuCont.classList.toggle("active");
     mobileMenu.classList.toggle("active");    
 });
+
+challengesLocal = [];
+
+async function fetchChallenges() {
+
+const res = await fetch('https://lernia-sjj-assignments.vercel.app/api/challenges');
+const data = await res.json();
+
+data.challenges.forEach(challenge => {
+  console.log(challenge);
+});
+
+
+
+}
+
+
